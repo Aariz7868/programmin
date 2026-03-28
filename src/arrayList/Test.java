4a0d74c76754a8d712bbd7c4024c4a10e246c4a2
Aariz7868 <mohdaariz991@gmail.com>
2026-03-28 12:38:47 +0530

    Update Test.java

--- src/arrayList/Test.java
+++ src/arrayList/Test.java
@@ -2,7 +2,9 @@ package arrayList;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		WebElement ele1 = driver.findElement(by.id("Aariz"));
+		WebElement ele2 = driver.findElement(by.id("Aariz"));
+		WebElement ele3 = driver.findElement(by.id("Aariz"));
 	}
 
 }
